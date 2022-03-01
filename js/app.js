c204fa37689e03a0d2ed363a2085832b02ec76fa
Dipok Roy <roydipok022@gmail.com>
2022-03-01 12:11:50 +0600

    load phones api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-
+//load phones api
+const loadPhones = () => {
+  const searchPhone = document.getElementById("search-phone").value;
+  const searchText = searchPhone.toLowerCase();
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};
+loadPhones();
