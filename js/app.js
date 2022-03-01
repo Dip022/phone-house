a32fa942610fc332eb2afa6d9fa925b1360d6658
Dipok Roy <roydipok022@gmail.com>
2022-03-01 13:27:39 +0600

    display phones

--- js/app.js
+++ js/app.js
@@ -4,6 +4,26 @@ const loadPhones = () => {
   const searchText = searchPhone.toLowerCase();
   fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => displayPhones(data.data));
 };
-loadPhones();
+// display phones
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const div = document.createElement("div");
+    div.classList.add("col-12", "col-md-6", "col-lg-4");
+    div.innerHTML = `
+        <div class="card rounded">
+            <div class="p-4">
+                <img width="100%" src="${phone.image}" class="card-img-top" alt="" />
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand} ${phone.phone_name}</h5>
+                <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Learn more</button>
+            </div>
+        </div>
+    `;
+    phoneContainer.appendChild(div);
+  });
+};
