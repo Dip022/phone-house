0bec76722339050690a4b228aaff29d91f7a246a
Dipok Roy <roydipok022@gmail.com>
2022-03-01 15:12:42 +0600

    show phone info

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const loadPhones = () => {
   const searchPhone = document.getElementById("search-phone").value;
   const searchText = searchPhone.toLowerCase();
+  //   search input clear
+  document.getElementById("search-phone").value = "";
   fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then((res) => res.json())
     .then((data) => displayPhones(data.data));
@@ -9,6 +11,8 @@ const loadPhones = () => {
 // display phones
 const displayPhones = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
+  //   display phones clear
+  phoneContainer.textContent = "";
   phones.forEach((phone) => {
     console.log(phone);
     const div = document.createElement("div");
@@ -32,5 +36,27 @@ const phoneDetails = (phoneId) => {
   const phoneInfo = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
   fetch(phoneInfo)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => showPhoneInfo(data.data));
+};
+
+// show phone info
+const showPhoneInfo = (showInfo) => {
+  const loadInfo = document.getElementById("showPhoneInfo");
+  //   phone info clear
+  loadInfo.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("show-info");
+  div.innerHTML = `
+    <div class="p-4 text-center">
+        <img src="${showInfo.image}" class="card-img-top " alt="..." />
+    </div>
+    <p class="fw-bold">Brand: ${showInfo.brand}</p>
+    <p class="fw-bold">Display Size: ${showInfo.mainFeatures.displaySize}</p>
+    <p class="fw-bold">Memory: ${showInfo.mainFeatures.memory}</p>
+    <p class="fw-bold">Brand: ${
+      showInfo.releaseDate ? showInfo.releaseDate : "No Relesed Date Found!"
+    }</p>
+    
+  `;
+  loadInfo.appendChild(div);
 };
